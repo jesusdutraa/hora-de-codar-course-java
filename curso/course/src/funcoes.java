221632207f579ccd4ee1e03d4fbc0b1bd2ea1c50
jesusdutraa <gabrieljesusdev@gmail.com>
2025-06-01 21:14:14 -0300

    adicionando novo conteúdo de funções

---
+++ curso/course/src/funcoes.java
@@ -0,0 +1,12 @@
+public class funcoes {
+  public static void main(String[] args) {
+    saudacao();
+  }
+  
+
+  // public => Nivel de acesso (público) => static => não precisa instanciar classe para executar, void => tipo de retorno (vazio)
+  // Nome da função _ (os argumentos) _ bloco {}
+  public static void saudacao(){
+    System.out.println("Olá, esta é minha primeira função em Java.");
+  }
+}
